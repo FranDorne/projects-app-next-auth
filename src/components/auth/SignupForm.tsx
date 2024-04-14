f5b96c4e84489440e6d4d96cbe12fa95e7509d5a
FranDorne <francodornelesux@gmail.com>
2024-04-14 19:15:19 -0300

    login and register react hook form

--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -1,33 +1,111 @@
-import { EnvelopeClosedIcon, LockClosedIcon, PersonIcon } from "@radix-ui/react-icons";
+"use client";
+
+import {
+  EnvelopeClosedIcon,
+  LockClosedIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
 import { Button, Flex, TextField } from "@radix-ui/themes";
 import React from "react";
+import { useForm, Controller } from "react-hook-form";
 
 function SignUpForm() {
+  const { control, handleSubmit } = useForm({
+    values: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <Flex direction='column' gap='2'>
-      <label htmlFor="name">Name</label>
-      <TextField.Root type="text" placeholder="Write your name" autoFocus>
-        <TextField.Slot>
-          <PersonIcon width="16" height="16" />
-        </TextField.Slot>
-      </TextField.Root>
+    <form onSubmit={onSubmit}>
+      <Flex direction="column" gap="2">
+        <label htmlFor="name">Name</label>
+        <Controller
+          name="name"
+          control={control}
+          rules={{
+            required: {
+              message: "Name is required",
+              value: true,
+            },
+          }}
+          render={({field}) => {
+            return (
+              <TextField.Root
+                type="text"
+                placeholder="Write your name"
+                autoFocus
+                {...field}
+              >
+                <TextField.Slot>
+                  <PersonIcon width="16" height="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            );
+          }}
+        />
 
-      <label htmlFor="email">Email</label>
-      <TextField.Root type="email" placeholder="email@domain.com" >
-        <TextField.Slot>
-          <EnvelopeClosedIcon width="16" height="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        <label htmlFor="email">Email</label>
 
-      <label htmlFor="password">Password</label>
-      <TextField.Root type="password" placeholder="*******">
-        <TextField.Slot>
-          <LockClosedIcon width="16" height="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: {
+              message: "Email is required",
+              value: true,
+            },
+          }}
+          render={({field}) => {
+            return (
+              <TextField.Root
+                type="email"
+                placeholder="email@domain.com"
+                {...field}
+              >
+                <TextField.Slot>
+                  <EnvelopeClosedIcon width="16" height="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            );
+          }}
+        />
 
-      <Button>Sign in</Button>
-    </Flex>
+        <label htmlFor="password">Password</label>
+
+        <Controller
+          name="password"
+          control={control}
+          rules={{
+            required: {
+              message: "Password is required",
+              value: true,
+            },
+            minLength: {
+              message: "Password must be at least 6 characters",
+              value: 6,
+            },
+          }}
+          render={({field}) => {
+            return (
+              <TextField.Root type="password" placeholder="*******" {...field}>
+                <TextField.Slot>
+                  <LockClosedIcon width="16" height="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            );
+          }}
+        />
+
+        <Button type="submit">Sign up</Button>
+      </Flex>
+    </form>
   );
 }
 
